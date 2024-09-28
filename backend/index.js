db988a50ac916994b18d5c2dbcf94ab6bf19aa7e
Quang Huy <nguyenquanghuya3kd@gmail.com>
2024-09-29 05:50:35 +0700

    Add Cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookiesparser());
 app.use("/api/auth", authRoute);
